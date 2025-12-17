bcb94e14e4e662cf1351b48aa7e57217c9007990
HOME <erwinlagos0506@gmail.com>
2025-12-18 00:46:05 +0100

    mañana sigo q me da pereza y quiero mimir

--- src/gui/Vista.java
+++ src/gui/Vista.java
@@ -5,5 +5,20 @@ import javax.swing.*;
 public class Vista {
     private JPanel panel1;
     private JTabbedPane tabbedPane1;
-    private JPanel PanelLibro;
+    private JPanel PanelCanciones;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JTextField textField2;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JSlider slider1;
+    private JCheckBox dembowCheckBox;
+    private JCheckBox inglesCheckBox;
+    private JCheckBox españolCheckBox;
+    private JButton añadirButton;
+    private JButton modificarButton;
+    private JButton eliminarButton;
+    private JTable tablaCanciones;
 }
